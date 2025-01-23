7d9a7c0d9e074bf63502c350fc5d085d56c73771
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-01-23 15:29:27 +0000

    create post page configs

--- blogWeb/src/app/app-routing.module.ts
+++ blogWeb/src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatePostComponent } from './pages/create-post/create-post.component';
 // import { HomeComponent } from './AngularMaterialModule'; // Replace with your actual components
 // import { AboutComponent } from './app.component.spec';
 
 const routes: Routes = [
-  { path: '', component:  }, // Default route
-  { path: 'about', component:  }, // Example route
-  { path: '**', redirectTo: '' }, // Wildcard route for 404
+  { path: 'create-post', component:  CreatePostComponent }, // Default route
+//   { path: 'about', component:  }, // Example route
+//   { path: '**', redirectTo: '' }, // Wildcard route for 404
 ];
 
 @NgModule({
