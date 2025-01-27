f8abbab4237fde06462c813303036bc41586d9bb
Maxwell Adomako <83253800+MaxiFine@users.noreply.github.com>
2025-01-27 11:23:10 +0000

    configs to deploy after build

--- blogWeb/src/app/app.module.ts
+++ blogWeb/src/app/app.module.ts
@@ -11,9 +11,7 @@ import { MaterialModules } from './AngularMaterialModule';
 
 
 import { Component } from '@angular/core';
-
 import { RouterOutlet } from '@angular/router';
-
 import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
 @NgModule({
