836ec8680dd896a6b0e0cab5d7be181ac40114ff
Maxwell Adomako <gibboel5@gmail.com>
2025-11-28 16:21:05 +0000

    refactor routing and enhance global styles for improved UI

--- blogWeb/src/app/app-routing.module.ts
+++ blogWeb/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreatePostComponent } from './pages/create-post/create-post.component';
-// import { HomeComponent } from './AngularMaterialModule'; // Replace with your actual components
-// import { AboutComponent } from './app.component.spec';
+import { BlogListComponent } from './pages/blog-list/blog-list.component';
+import { BlogDetailComponent } from './pages/blog-detail/blog-detail.component';
 
 const routes: Routes = [
-  { path: 'create-post', component:  CreatePostComponent }, // Default route
-//   { path: 'about', component:  }, // Example route
-//   { path: '**', redirectTo: '' }, // Wildcard route for 404
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
+  { path: 'posts', component: BlogListComponent },
+  { path: 'blog/:id', component: BlogDetailComponent },
+  { path: 'create-post', component: CreatePostComponent },
+  { path: '**', redirectTo: '/posts' }
 ];
 
 @NgModule({
