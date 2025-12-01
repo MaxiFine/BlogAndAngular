4e639804060ce20c9a9f84161058e3c7e09ce302
Maxwell Adomako <gibboel5@gmail.com>
2025-12-01 02:35:18 +0000

    update app configuration and routes to include HTTP client and define application routes

--- blogWeb/src/app/app.routes.ts
+++ blogWeb/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { BlogListComponent } from './pages/blog-list/blog-list.component';
+import { BlogDetailComponent } from './pages/blog-detail/blog-detail.component';
+import { CreatePostComponent } from './pages/create-post/create-post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },
+  { path: 'posts', component: BlogListComponent },
+  { path: 'blog/:id', component: BlogDetailComponent },
+  { path: 'create-post', component: CreatePostComponent },
+  { path: '**', redirectTo: '/posts' }
+];
